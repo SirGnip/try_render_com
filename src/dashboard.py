62213bf0ad862de82604e9e1e14619291b19ef75
SirGnip <SirGnip@users.noreply.github.com>
2024-11-19 17:59:58 -0600

    Add table to Panel dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,11 +1,18 @@
+import pandas as pd
 import panel as pn
 
-pn.extension()
+pn.extension("tabulator")
 
 hdr = pn.pane.Markdown("## Hello world")
 btn = pn.widgets.Button(name="Refresh", button_type="success")
+df = pd.DataFrame([('foo', 99), ('bar', 42)], columns=['name', 'va'])
+table = pn.widgets.Tabulator(df)
+def on_click(e):
+    print("clicked button", e)
+btn.on_click(on_click)
 
 pn.layout.Column(
     hdr,
-    btn
+    btn,
+    table,
 ).servable()
