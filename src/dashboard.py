9b4cd2a8d4a8b90a40fd775bbbb1ee0d61b48dcb
SirGnip <SirGnip@users.noreply.github.com>
2024-11-19 18:08:12 -0600

    Add dynamic table

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,3 +1,4 @@
+import random
 import pandas as pd
 import panel as pn
 
@@ -7,8 +8,17 @@ hdr = pn.pane.Markdown("## Hello world")
 btn = pn.widgets.Button(name="Refresh", button_type="success")
 df = pd.DataFrame([('foo', 99), ('bar', 42)], columns=['name', 'va'])
 table = pn.widgets.Tabulator(df)
+
+def make_df():
+    names = ("foo", "bar", "zip", "linux", "bob", "mary")
+    rows = []
+    for i in range(random.randint(2, 8)):
+        rows.append([random.choice(names), random.randint(1, 99)])
+    return pd.DataFrame(rows, columns=["name", "val"])
+
 def on_click(e):
     print("clicked button", e)
+    table.value = make_df()
 btn.on_click(on_click)
 
 pn.layout.Column(
